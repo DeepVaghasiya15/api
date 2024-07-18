a7f71dfc3af97349fabd74dfc2c9ea5fb3215dd9
Deep <deepvaghasiya15@gmail.com>
2024-07-18 18:53:20 +0530

    gunicord

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from config import MONGO_URI
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all domains
 
-client = MongoClient(MONGO_URI)
-db = client.get_database()
-property_collection = db.properties
+def get_property_collection():
+    client = MongoClient(MONGO_URI)
+    db = client.get_database()
+    return db.properties
 
 @app.route('/properties/', methods=['GET'])
 def get_properties():
+    property_collection = get_property_collection()
     properties = list(property_collection.find({}, {'_id': 0}))
     return jsonify(properties)
 
 if __name__ == '__main__':
-    app.run(port=7777)  # Ensure this port matches the one you expose with Ngrok
+    app.run(port=7777)
